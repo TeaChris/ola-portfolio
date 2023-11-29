d765b562e518cc2134e50000693f5c6df2b7579d
Bermuda <olasunkanmiboluwatife110@gmail.com>
2023-11-29 10:10:44 +0100

    mobile nav fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,7 @@
 import { FC } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import MobileNav from './MobileNav'
-import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-} from './ui/sheet'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 import { Menu } from 'lucide-react'
 
 interface NavbarProps {}
@@ -34,9 +29,9 @@ const Navbar: FC<NavbarProps> = ({}) => {
   return (
     <nav className="w-full h-[6%] flex items-center justify-center top-0 left-0 fixed bg-body z-20 border-0 lg:border-b border-textMuted shadow-sm shadow-black lg:shadow-none">
       <div className="w-full flex items-start justify-between h-full px-2 lg:px-0">
-        <div className="w-[40%] h-full flex items-center">
-          <div className="w-[25%] h-full border-r border-textMuted flex items-center justify-center">
-            <h3 className="text-textSecondary text-lg sm:text-xl font-bold">
+        <div className="lg:w-[40%] h-full flex items-center">
+          <div className="w-fit lg:w-[25%] h-full border-0 lg:border-r lg:border-textMuted flex items-center justify-center">
+            <h3 className="text-textSecondary text-2xl sm:text-xl font-bold">
               Bermuda
             </h3>
           </div>
@@ -56,7 +51,7 @@ const Navbar: FC<NavbarProps> = ({}) => {
           </div>
         </div>
         <div
-          className={`${
+          className={`hidden lg:flex ${
             pathname === '/contact' ? 'border-b-2 border-textOrange' : ''
           } w-[10%] h-full border-l border-textMuted flex items-center justify-center`}
         >
@@ -64,22 +59,26 @@ const Navbar: FC<NavbarProps> = ({}) => {
             _contact
           </Link>
         </div>
-        <div className="lg:hidden md:block">
+        <div className="lg:hidden md:block w-10 h-full items-center justify-center">
           <Sheet>
-            <SheetTrigger className="group -m-2 flex items-center p-2">
+            <SheetTrigger className="h-full w-10 flex items-center justify-center">
               <Menu className="h-6 w-6 flex-shrink-0 text-textMuted" />
             </SheetTrigger>
             {/* ts-ignore */}
             <SheetContent className="flex items-center justify-center">
-              <div className="w-fit h-fit flex flex-col items-center gap-4">
+              <div className="w-fit h-fit flex flex-col items-center gap-8">
                 {nav.map((item) => (
-                  <Link key={item.label} href={item.href} className="text-base text-textMuted">
+                  <Link
+                    key={item.label}
+                    href={item.href}
+                    className="text-2xl text-textMuted"
+                  >
                     {item.label}
                   </Link>
                 ))}
-                <Link href={'/contact'} className="text-base text-textMuted">
-                    _contact
-                  </Link>
+                <Link href={'/contact'} className="text-2xl text-textMuted">
+                  _contact
+                </Link>
               </div>
             </SheetContent>
           </Sheet>
